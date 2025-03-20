9ec7ce2563ebafa6ba937bcd29c901d3589fe2ee
arunrocker <m.arunkumarmar12@gmail.com>
2025-03-20 21:49:23 +0530

    Made RabbitMq Configuration based

--- src/main/java/com/arun/chat/config/WebSocketConfig.java
+++ src/main/java/com/arun/chat/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.arun.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,14 +10,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
-
+	@Value("{spring.rabbitmq.host}")
+	private String hostName;
+	@Value("{spring.rabbitmq.port}")
+	private String port;
+	@Value("{spring.rabbitmq.username}")
+	private String userName;
+	@Value("{spring.rabbitmq.password}")
+	private String password;
+	
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 			registry.addEndpoint("/websocketChat").withSockJS(); // Just for webSocket Connection
 	}
 	
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("/app"); // For every websocket Request should have this prefix
-		registry.enableStompBrokerRelay("/topic").setRelayHost("localhost").setRelayPort(61613).setClientLogin("guest")
-		.setClientPasscode("guest");
+		registry.enableStompBrokerRelay("/topic").setRelayHost(hostName).setRelayPort(Integer.parseInt(port)).setClientLogin(userName)
+		.setClientPasscode(password);
 	}
 }
